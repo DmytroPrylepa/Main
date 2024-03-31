1464d4258de38a73106a0968061106322633da88
DmytroPrylepa <prilepa.dm@gmail.com>
2024-03-31 20:41:22 +0100

    methods to change and delete user

--- src/main/UserManager.java
+++ src/main/UserManager.java
@@ -6,6 +6,7 @@ package main;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -73,5 +74,64 @@ public class UserManager {
             System.out.println("Failed to delete user.");
             e.printStackTrace();
         }
+    }
+    // method to find a user by their username
+    public User findUserByUsername(String username) {
+        try {
+            String query = "SELECT * FROM User WHERE username = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int userId = resultSet.getInt("user_id");
+                String password = resultSet.getString("password");
+                String role = resultSet.getString("role");
+                return new User(userId, username, password, role);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to find user.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+	
+	// method to change the password of a user
+    public void changePassword(String username, String newPassword) {
+        try {
+            String query = "UPDATE User SET password = ? WHERE username = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, newPassword);
+            statement.setString(2, username);
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Password changed successfully.");
+            } else {
+                System.out.println("User not found.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to change password.");
+            e.printStackTrace();
+        }
+    }
+	
+	// method to authenticate a user based on username and password
+    public boolean authenticateUser(String username, String password) {
+        try {
+            String query = "SELECT * FROM User WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            System.out.println("Failed to authenticate user.");
+            e.printStackTrace();
+        }
+        return false;
+    }
+	
+	// method to get the database connection object
+    public Connection getConnection() {
+        return connection;
     }
 }
