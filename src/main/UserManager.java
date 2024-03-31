a7a931767db22d59994ec9b87e3d4b059058dbd0
DmytroPrylepa <prilepa.dm@gmail.com>
2024-03-31 20:38:57 +0100

    method to add a new user to the database

--- src/main/UserManager.java
+++ src/main/UserManager.java
@@ -4,10 +4,36 @@
  */
 package main;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author upgra
  */
 public class UserManager {
-    
+    private Connection connection;
+
+    public UserManager(Connection connection) {
+        this.connection = connection;
+    }
+	
+	
+	
+	// method to add a new user to the database
+    public void addUser(String username, String password, String role) {
+        try {
+            String query = "INSERT INTO User (username, password, role) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.setString(3, role);
+            statement.executeUpdate();
+            System.out.println("User added successfully.");
+        } catch (SQLException e) {
+            System.out.println("Failed to add user.");
+            e.printStackTrace();
+        }
+    }
 }
